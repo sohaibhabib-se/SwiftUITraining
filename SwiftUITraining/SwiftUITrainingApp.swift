24be3e77a7642dfed55f2631c4433f9512583eb6
Sohaib Habib <sohaibhabib8@gmail.com>
2024-06-11 18:52:36 +0500

    Initial Commit

---
+++ SwiftUITraining/SwiftUITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainingApp.swift
+//  SwiftUITraining
+//
+//  Created by Muhammad Sohaib on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
